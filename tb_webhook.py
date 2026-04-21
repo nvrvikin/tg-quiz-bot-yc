3005902a5e6c864761a722f61c78f6f60aab95cd
Vladislav Naraikin <randalv@yandex.ru>
2026-04-21 17:20:47 +0700

    set push github action for yc deploing

--- tb_webhook.py
+++ tb_webhook.py
@@ -5,22 +5,14 @@ import handlers
 
 import json
 
-
-# Включаем логирование, чтобы не пропустить важные сообщения
+# Включаем логирование, чтобы не пропустить важные сообщения 
 logging.basicConfig(level=logging.INFO)
-
-# Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
-# API_TOKEN = '6353924149:AAELVCpnt0U4P_6lW76dm8EaCrqy91--OSQ'
-
 
 API_TOKEN = os.getenv("API_TOKEN")
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 dp.include_router(handlers.router)
-
-
-
 
 async def process_event(event):
 
@@ -29,9 +21,8 @@ async def process_event(event):
 
 async def webhook(event, context):
     if event['httpMethod'] == 'POST':
-        # Bot and dispatcher initialization
-        # Объект бота
-
+        # Bot and dispatcher initialization 
+        # Объект бота 
 
         await process_event(event)
         return {'statusCode': 200, 'body': 'ok'}
