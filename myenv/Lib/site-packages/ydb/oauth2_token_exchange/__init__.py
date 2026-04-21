2286264fd43d970645cf4808c0175f954f2b2237
Vladislav Naraikin <randalv@yandex.ru>
2026-04-21 18:48:50 +0700

    Delete myenv directory

--- myenv/Lib/site-packages/ydb/oauth2_token_exchange/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from .token_source import FixedTokenSource  # noqa
-from .token_source import JwtTokenSource  # noqa
-from .token_exchange import Oauth2TokenExchangeCredentials  # noqa
